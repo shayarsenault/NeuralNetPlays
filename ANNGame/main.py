91b77ac43c6bb257d9cfd6d9ee3542d9628101ed
shayarsenault <shayarsenault@yahoo.com>
2017-11-24 23:13:26 -0500

    Fixed
    -Fixed code: Had 2 entity files
    -Kinda added enemy mechanic
    -Added Game Over function kinda
    -TODO: Fix player glitching through base

--- ANNGame/main.py
+++ ANNGame/main.py
@@ -1,6 +1,12 @@
-import pygame
-from random import randrange
+
 from window import Window
+from entities import Player, Base, Enemy
+import pygame
+import time
+from random import *
+
+pygame.init()
+
 
 # Color Declaration
 BLACK = (0,0,0)
@@ -10,48 +16,62 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 
-class Base(Window):
-	def __init__(self, win):
-		self.x = 0
-		self.y = win.height/2+25
-	def update(self, win):
-		pygame.draw.rect(win.display, BLUE, (self.x, self.y, win.width, 10))
-		
+running = True
+fps = 30
 
-class Player(Base):
-	def __init__(self, win):
-		self.x = 75
-		self.y = win.height/2
-		self.yc = 0 
 
-		self.width = 25
-		self.height = 25
 
-	def update(self, win):
-		pygame.draw.rect(win.display, BLACK, (self.x, self.y, self.width, self.height))
+window = Window(800,600)
+base = Base(window)
+player1 = Player(window)
 
-	def move(self):
-		self.yc = -10
 
-class Enemy(Window):
-	def __init__(self, win):
-		self.color = RED
-		self.width = randrange(25, 150)
-		self.height = 25
-		self.x = win.width - self.width
-		self.y = win.height/2
+clock = pygame.time.Clock()
 
-		self.speed = 10
+rects = []
 
-		self.enemies = []
+for count in range(10):
+	color = (randint(0,255), randint(0,255), randint(0,255))
+	    
+	random_size = (51, 25)
 
-	def update(self, win):
-		pygame.draw.rect(win.display, self.color, (self.x, self.y, self.width, self.height))
-		
+	enemy = Enemy(window, color, random_size)
+	rects.append(enemy)
 
+while running:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP and player1.y+player1.height == base.y:
+				player1.move()
+	if player1.y == 100:
+		player1.yc = 10
+
+	elif player1.y+25 == base.y and event.type == pygame.KEYUP:
+		player1.yc = 0
+
+	player1.y += player1.yc
 	
-		
+	for r in rects:
+		enemy.x -= enemy.speed
+		if enemy.x == 0:
+			enemy.speed = 0
+			r.update(window)
+		elif enemy.x == player1.y and player1.x+25:
+			enemy.speed = 0 
+			window.game_over()
 
+	pygame.display.update()
+
+	window.display.fill(WHITE)
+
+	player1.update(window)
+	base.update(window)
+	enemy.update(window)
+
+	clock.tick(fps)	
 
 
 
