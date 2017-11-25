cf84d0af5a43c79e9a87d9d6cd0f557c6aef02d9
shayarsenault <shayarsenault@yahoo.com>
2017-11-24 23:15:15 -0500

    Redid Base and Enemy
    -Changed Base thickness to 20 instead of 10
    -Redid the enemy attributes to fit with the movement functionality in main.py

--- ANNGame/entities.py
+++ ANNGame/entities.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 from random import randrange
 from window import Window
 
@@ -15,7 +16,7 @@ class Base(Window):
 		self.x = 0
 		self.y = win.height/2+25
 	def update(self, win):
-		pygame.draw.rect(win.display, BLUE, (self.x, self.y, win.width, 10))
+		pygame.draw.rect(win.display, BLUE, (self.x, self.y, win.width, 20))
 		
 
 class Player(Base):
@@ -27,6 +28,8 @@ class Player(Base):
 		self.width = 25
 		self.height = 25
 
+		self.area = self.width * self.height
+
 	def update(self, win):
 		pygame.draw.rect(win.display, BLACK, (self.x, self.y, self.width, self.height))
 
@@ -34,22 +37,21 @@ class Player(Base):
 		self.yc = -10
 
 class Enemy(Window):
-	def __init__(self, win):
-		self.color = RED
-		self.width = randrange(25, 150)
-		self.height = 25
-		self.x = win.width - self.width
+	def __init__(self, win, color, size):
+		
+		self.color = color
+		self.size = size
+		self.x = win.width
 		self.y = win.height/2
-
-		self.speed = 10
+		self.speed = 5
 
 		self.enemies = []
 
 	def update(self, win):
-		pygame.draw.rect(win.display, self.color, (self.x, self.y, self.width, self.height))
+		pygame.draw.rect(win.display, self.color, Rect((self.x, self.y), self.size))
 		
 	def move_and_gen(self):
-			
+		pass
 		
 
 
