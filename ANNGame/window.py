0e2143fb6b368600392f8f15ce2d56ba66eb1949
shayarsenault <shayarsenault@yahoo.com>
2017-11-24 23:13:42 -0500

    Added Font and Game Over function

--- ANNGame/window.py
+++ ANNGame/window.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 
 # Color Declaration
@@ -17,6 +18,12 @@ class Window:
 		
 		self.display = pygame.display.set_mode((self.width, self.height))
 		
+		self.font = pygame.font.SysFont(None, 30)	
+		self.gameOverText = self.font.render('Game Over', False, (0,0,0))
+
 		pygame.display.set_caption('A neural net')
 		
 		pygame.display.update()
+	def game_over(self):
+
+		self.display.blit(self.gameOverText, [self.width/2-25, self.height/2-100])
