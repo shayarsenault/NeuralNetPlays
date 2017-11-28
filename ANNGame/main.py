5b0d6437fdd4a81d09cf4cd9289ba1d308ed46e4
shayarsenault <shayarsenault@yahoo.com>
2017-11-28 17:46:23 -0500

    changed some logic

--- ANNGame/main.py
+++ ANNGame/main.py
@@ -31,41 +31,50 @@ player1 = Player(window)
 clock = pygame.time.Clock()
 
 rects = []
+size = (randint(25, 200), 25)
 
 for count in range(10):
 	color = (randint(0,255), randint(0,255), randint(0,255))
 	    
-	random_size = (51, 25)
+	random_size = (randint(25, 100), 25)
 
 	enemy = Enemy(window, color, random_size)
 	rects.append(enemy)
 
 while running:
-	# TODO: FIX PLAYER GLITCHING THROUGH BASE
+	# TODO: FIX PLAYER GLITCHING THROUGH BASE ***
+	# TODO: FIX ENEMY SPAWN OBJECT
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP and player1.y+player1.height == base.y:
-
 				player1.move()
-	if player1.y == 100:
+
+				if player1.y+25 != base.y:
+					player1.y += player1.yc
+				elif player1.y+25 == base.y:
+					player1.yc = 0
+
+	# Determines if Player has collided with the base, if true, then the player stays of then
+	if player1.y == player1.jump_threshold:
 		player1.yc = 10
-
-	elif player1.y+25 == base.y and event.type == pygame.KEYUP:
-		player1.yc = 0
-
-	player1.y += player1.yc
+	elif player1.y+25 == base.y:
+		if event.type == pygame.KEYDOWN:
+			player1.yc = 0
 	
+	
+
 	for r in rects:
 		enemy.x -= enemy.speed
 		if enemy.x == 0:
-			enemy.generate_new(window)
-		elif enemy.x == player1.x+25 and enemy.y == player1.y:
+			enemy.generate_new(window, size)
+		elif enemy.x == player1.x+player1.width and enemy.y == player1.y:
 			enemy.speed = 0 
 			window.game_over()
-			print(enemy.x, enemy.y, player1.x, player1.y)
+			exit()
 
+			
 	pygame.display.update()
 
 	window.display.fill(WHITE)
