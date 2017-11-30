ec7949cd2bb8b54e39942bffccf06d75739f828b
shayarsenault <shayarsenault@yahoo.com>
2017-11-29 21:51:26 -0500

    Fixed the player being able to jump after the game ended, fixed the enemy spawning with the same width

--- ANNGame/main.py
+++ ANNGame/main.py
@@ -21,56 +21,63 @@ BLUE = (0,0,255)
 running = True
 fps = 30
 
-
-
 window = Window(800,600)
 base = Base(window)
 player1 = Player(window)
+font = pygame.font.SysFont(None, 30)
 
 
 clock = pygame.time.Clock()
 
 rects = []
-size = (randint(25, 200), 25)
+
+
+score = 0
+
+
 
 for count in range(10):
-	color = (randint(0,255), randint(0,255), randint(0,255))
-	    
+	color = (randint(0,255), randint(0,255), randint(0,255))    
 	random_size = (randint(25, 100), 25)
-
 	enemy = Enemy(window, color, random_size)
 	rects.append(enemy)
 
+
 while running:
-	# TODO: FIX PLAYER GLITCHING THROUGH BASE *
-	# TODO: FIX ENEMY SPAWN OBJECT
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP and player1.yc != 10:
+			if event.key == pygame.K_UP and player1.yc != 10 and enemy.x != player1.x+player1.width:
 				player1.move()
 
-	
-
-	# Determines if Player has collided with the base, if true, then the player stays of then
-	if player1.y == player1.jump_threshold :		
+	# Determines if Player has collided with the base, if true, then the player stays on it
+	if player1.y == player1.jump_threshold:		
 		player1.yc = 10
 	elif player1.yc == 10 and player1.y+player1.height == base.y:
 		player1.yc = 0
 
 	player1.y += player1.yc
 
-	for r in rects:
-		enemy.x -= enemy.speed
-		if enemy.x == 0:
-			enemy.generate_new(window, size)
-		elif enemy.x == player1.x+player1.width and enemy.y == player1.y:
-			enemy.speed = 0 
-			window.game_over()
-			
 
-			
+# Enemy spawn logic
+	for r in rects:
+		sx = randint(25, 200)
+		size = (sx, 25)
+		enemy.x -= enemy.speed
+	if enemy.x == 0:
+		enemy.generate_new(window, size)
+		print(sx)
+		score += 1
+	
+	elif enemy.x == player1.x+player1.width and enemy.y == player1.y:
+		enemy.speed = 0
+		window.game_over()
+
+	score_txt = font.render('Score: ' + str(score), False, (0,0,0))
+
+	window.display.blit(score_txt, [10, 10])
+
 	pygame.display.update()
 
 	window.display.fill(WHITE)
